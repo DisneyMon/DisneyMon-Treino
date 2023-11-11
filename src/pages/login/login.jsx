6ee8cce39f5f8f1080d01e6a7e6bb436752821ed
RodrigoWiizz <83847745+RodrigoWiizz@users.noreply.github.com>
2023-11-11 16:35:46 -0300

    Login quase pronto

--- src/pages/login/login.jsx
+++ src/pages/login/login.jsx
@@ -1,11 +1,27 @@
 import React from 'react';
 import './login.scss';
 
-import Input from "../../components/login-cadastro/input/input";
-import Button from "../../components/login-cadastro/button/button";
 import { Link } from 'react-router-dom';
+import axios from 'axios';
+import { useState } from 'react';
 
 export default function Login() {
+
+    const [email, setEmail] = useState("");
+    const [senha, setSenha] = useState("");
+    const [nome, setNome] = useState("");
+
+    async function login(event){
+        event.preventDefault();
+        let response = await axios.get(`http://localhost:8080/usuarios/login?email=${email}&senha=${senha}`)
+        if(response.status === 404){
+            alert(`Erro ao tentar logar, email ou senha incorretos`)
+        }
+        else{
+            alert(`Usuário ${response.data.nome} logado com sucesso`)
+        }
+    }
+
     return (
 
         <div className="login-page">
@@ -19,9 +35,9 @@ export default function Login() {
                     <form className="form-login">
 
                         <h1 className="h1-login">Login</h1>
-                        <Input title="E-mail" type="text"/>
-                        <Input title="Senha" type="password"/>
-                        <Button action="Entrar"/>
+                        <input value={email} onChange={event => setEmail(event.target.value)} type="text"/>
+                        <input  type="password" value={senha} onChange={event => setSenha(event.target.value)}/>
+                        <button onClick={login} action="Entrar"/>
                         <Link className="a-login" href="/cadastro">Não possui conta? Cadastre-se aqui</Link>
 
                     </form>
