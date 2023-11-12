3a1ef0f5e79caab63ca7671e85481ca85e3c5881
Leonardo Ravanelli <leonardo.lrc17@gmail.com>
2023-11-12 12:52:17 -0300

    Pagina Produtos Pronta

--- front/src/pages/produtos/produtos.jsx
+++ front/src/pages/produtos/produtos.jsx
@@ -1,5 +1,6 @@
 import './produtos.scss'
 import Header from '../../components/global/header/header'
+import Card from '../../components/global/card/card'
 
 export default function Produtos() {
 
@@ -14,13 +15,32 @@ export default function Produtos() {
                 <figure className="figure-produtos-imgs">
                     <img className='img-produtos img-produtos-picachu' src="/assets/images/produtos/picachu-produtos.webp" alt="Foto Pikachu" />
                     <article className="article-produtos-titulo">
-                        <h1>DisneyMon</h1>
-                        <h4>Produtos disneyMon</h4>
+                        <h1 className='h1-produtos'>DisneyMon</h1>
+                        <h4 className='h4-produtos'>Produtos</h4>
                     </article>
                     <img className='img-produtos img-produtos-mickey' src="/assets/images/produtos/mickey-produtos.webp" alt="" />
                 </figure>
-
             </header>
+
+            <main className="main-produtos">
+                <section className="section-produtos">
+
+                    <Card/>
+                    <Card/>
+                    <Card/>
+                    <Card/>
+                    <Card/>
+                    <Card/>
+                    <Card/>
+                    <Card/>
+                    <Card/>
+                    <Card/>
+                    <Card/>
+                    <Card/>
+                    <Card/>
+
+                </section>
+            </main>
 
         </div>
 
